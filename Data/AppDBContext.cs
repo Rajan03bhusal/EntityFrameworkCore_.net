575f30ae24b6ae095445992be785523f8add515f
Rajan Bhusal <rjnbhusal03@gmail.com>
2024-04-05 17:15:48 +0545

    EFC

---
+++ Data/AppDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EntityFramework.Data
+{
+    public class AppDBContext  :DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) :base(options)
+        { 
+
+        }
+        public DbSet<Book> Book { get; set; }
+        public DbSet<Language> Language { get; set; }
+       
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using EntityFramework.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configure the DbContext
+builder.Services.AddDbContext<AppDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AppDb")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
